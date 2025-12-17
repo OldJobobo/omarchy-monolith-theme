8aa3f8591f6d5afe018cd2bb386a53cf02071598
J. S. Brown <jsbrown7@gmail.com>
2025-12-17 10:09:54 -0800

    switched to Rose Pine Moon Color Scheme

--- neovim.lua
+++ neovim.lua
@@ -7,24 +7,24 @@ return {
             disable_italics = false,
             colors = {
                 -- Monotone shades (base00-base07)
-                base00 = "#100e11", -- Default background
-                base01 = "#ae9bb0", -- Lighter background (status bars)
-                base02 = "#100e11", -- Selection background
-                base03 = "#ae9bb0", -- Comments, invisibles
-                base04 = "#faf9fa", -- Dark foreground
-                base05 = "#faf9f9", -- Default foreground
-                base06 = "#faf9f9", -- Light foreground
-                base07 = "#faf9fa", -- Light background
+                base00 = "#232136", -- Base
+                base01 = "#2a273f", -- Surface
+                base02 = "#393552", -- Overlay / selection
+                base03 = "#6e6a86", -- Muted / comments
+                base04 = "#908caa", -- Subtle
+                base05 = "#e0def4", -- Text
+                base06 = "#e0def4", -- Light foreground
+                base07 = "#e0def4", -- Light background
 
                 -- Accent colors (base08-base0F)
-                base08 = "#e6b2b8", -- Variables, errors, red
-                base09 = "#e8b0b7", -- Integers, constants, orange
-                base0A = "#e3cab5", -- Classes, types, yellow
-                base0B = "#78c0d3", -- Strings, green
-                base0C = "#7ebcce", -- Support, regex, cyan
-                base0D = "#7e7ece", -- Functions, keywords, blue
-                base0E = "#e1afe1", -- Keywords, storage, magenta
-                base0F = "#e3c9b5", -- Deprecated, brown/yellow
+                base08 = "#eb6f92", -- Love
+                base09 = "#f6c177", -- Gold
+                base0A = "#ea9a97", -- Rose
+                base0B = "#3e8fb0", -- Pine
+                base0C = "#9ccfd8", -- Foam
+                base0D = "#c4a7e7", -- Iris
+                base0E = "#ea9a97", -- Rose (alt)
+                base0F = "#56526e", -- Highlight high
             },
         },
         config = function(_, opts)
